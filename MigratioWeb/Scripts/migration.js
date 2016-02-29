06352ae2efe56ccbe6ab54e2bd1a91309179724e
Boran Şimşek <simsekbo@gmail.com>
2016-02-29 19:01:04 +0100

    first change

--- MigratioWeb/Scripts/migration.js
+++ MigratioWeb/Scripts/migration.js
@@ -15,8 +15,6 @@ function loadMap() {
 }
 
 function getMessage() {
-    //var res = AjaxDeneme.GetMessage();
-    //$("#txtMessage").val(res.value);
     getPlaces();
     return false;
 }
@@ -31,7 +29,6 @@ function addMarkerOnClick(ev) {
 function getPlaces() {
     var clusterGroup = new L.MarkerClusterGroup();
     
-
     var res = AjaxDeneme.GetPlaces();
     var list = res.value;
     var myLayer = L.mapbox.featureLayer();
@@ -41,42 +38,10 @@ function getPlaces() {
     };
 
     list.forEach(function(place) {
-        //addPlace(place.Longitude, place.Latitude, place.Name);
         var resJson = testAddPlace(place.Longitude, place.Latitude, place.Name);
         geoJson.features.push(resJson);
     });
-    //var geoJson = {
-    //    type: 'FeatureCollection',
-    //    features: [{
-    //        type: 'Feature',
-    //        geometry: {
-    //            type: 'Point',
-    //            coordinates: [-19.512555, 63.530581666667]
-    //        },
-    //        properties: {
-    //            title: 'Skógafoss from Below',
-    //            'marker-size': 'small',
-    //            'marker-color': '#ffcc11',
-    //            'marker-symbol': 'camera',
-    //            url: 'http://phototastic.world/photo/skogafoss-from-below/'
-    //        }
-    //    }, {
-    //        type: 'Feature',
-    //        geometry: {
-    //            type: 'Point',
-    //            coordinates: [-23.31073, 64.92612]
-    //        },
-    //        properties: {
-    //            title: 'Kirkjufellsfoss on a Cloudy Day',
-    //            'marker-size': 'small',
-    //            'marker-color': '#ffcc11',
-    //            'marker-symbol': 'camera',
-    //            url: 'http://phototastic.world/photo/kirkjufellsfoss-on-a-cloudy-day/'
-    //        }
-    //    }
-    //      /* more Features in the same format ... */
-    //    ]
-    //};
+
     myLayer.setGeoJSON(geoJson);
     clusterGroup.addLayer(myLayer);
     map.addLayer(clusterGroup);
@@ -87,13 +52,9 @@ function getPlaces() {
 
 function addPlace(lat, lng, name) {
     L.mapbox.featureLayer({
-        // this feature is in the GeoJSON format: see geojson.org
-        // for the full specification
         type: 'Feature',
         geometry: {
             type: 'Point',
-            // coordinates here are in longitude, latitude order because
-            // x, y is the standard for GeoJSON and many formats
             coordinates: [
               lat,
               lng
@@ -102,8 +63,6 @@ function addPlace(lat, lng, name) {
         properties: {
             title: name,
             description: '',
-            // one can customize markers by adding simplestyle properties
-            // https://www.mapbox.com/guides/an-open-platform/#simplestyle
             'marker-size': 'large',
             'marker-color': '#BE9A6B'
         }
@@ -123,19 +82,5 @@ function testAddPlace(lat, lng, name) {
     res.properties.description = '';
     res.properties['marker-size'] = 'large';
     res.properties['marker-color'] = '#BE9A6B';
-    //var res = "type: 'Feature', " +
-    //    "geometry: {" +
-    //    "type: 'Point', " +
-    //    " coordinates: [" +
-    //    lat + "," +
-    //    lng +
-    //    "]" +
-    //    "}," +
-    //    "properties: {" +
-    //    "       title: " + name + ", " +
-    //    "       description: '', " +
-    //    "'marker-size': 'large', " +
-    //    "'marker-color': '#BE9A6B' " +
-    //    "}";
     return res;
 }
